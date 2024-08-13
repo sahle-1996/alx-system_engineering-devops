3b0f14fb672ecc722a1d6e8216cc678f646bd471
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-08-13 07:00:37 -0700

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-"""Module to recursively fetch hot post titles from a subreddit."""
+"""Module to recursively fetch hot post titles from a Reddit subreddit."""
 
-from requests import get
+import requests
 
 
-def fetch_all_hot_posts(subreddit, hot_posts=None, after_param=None):
-    """Recursively queries Reddit API to retrieve all hot post titles."""
-    if hot_posts is None:
-        hot_posts = []
+def recurse(subreddit, hot_list=None, after=None):
+    """Recursively retrieves titles of all hot posts from a given subreddit."""
+    if hot_list is None:
+        hot_list = []
 
     if not isinstance(subreddit, str) or not subreddit:
         return None
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    params = {'after': after_param, 'limit': 100}
+    params = {'after': after, 'limit': 100}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
-    response = get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code != 200:
         return None
 
     data = response.json().get('data', {})
-    after_param = data.get('after')
     posts = data.get('children', [])
+    after = data.get('after', None)
 
     for post in posts:
-        hot_posts.append(post.get('data', {}).get('title', 'No Title'))
+        title = post.get('data', {}).get('title', '')
+        hot_list.append(title)
 
-    if after_param:
-        return fetch_all_hot_posts(subreddit, hot_posts, after_param)
-
-    return hot_posts
+    if after:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
