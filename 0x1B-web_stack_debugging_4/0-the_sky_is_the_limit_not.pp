3688499bdbf0840db68f7dcf6fed3576bf8aa9ac
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-08-24 08:57:43 -0700

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,6 @@
-# Modifies Nginx settings to handle more traffic effectively.
+# Adjust Nginx configuration to increase request handling capacity.
 
-# Adjust the file limit for Nginx service
-exec { 'nginx_ulimit_update':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/bin:/usr/bin'
-} ->
-
-# Reload the Nginx service to apply changes
-exec { 'reload_nginx_service':
-  command => 'service nginx reload',
-  path    => '/sbin:/usr/sbin'
+exec { 'update_nginx_file_limit_and_restart':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && service nginx restart',
+  path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin']
 }
